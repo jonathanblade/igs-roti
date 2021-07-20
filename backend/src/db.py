e2747af1573c9fb00508eb4f0f8f566643dd2e67
Semen Syrovatskiy <semen.syrovatskiy@gmail.com>
2021-07-20 17:16:47 +0300

    Add Black code formatter

--- backend/src/db.py
+++ backend/src/db.py
@@ -10,27 +10,29 @@ DB_PASSWORD = os.environ.get("MONGO_DB_PASSWORD")
 
 
 class ROTIMapDB:
+    def __init__(self, user: str, password: str):
+        client = MongoClient(
+            f"mongodb+srv://{user}:{password}@Cluster0.9nhgn.mongodb.net/igs_roti_db?retryWrites=true&w=majority"
+        )
+        self.collection = client.igs_roti_db.map
 
-  def __init__(self, user: str, password: str):
-    client = MongoClient(f"mongodb+srv://{user}:{password}@Cluster0.9nhgn.mongodb.net/igs_roti_db?retryWrites=true&w=majority")
-    self.collection = client.igs_roti_db.map
+    def date2str(self, date: Union[str, datetime.date]) -> str:
+        if isinstance(date, datetime.date):
+            date = date.strftime(DATE_FORMAT)
+        return date
 
-  def date2str(self, date: Union[str, datetime.date]) -> str:
-    if isinstance(date, datetime.date):
-      date = date.strftime(DATE_FORMAT)
-    return date
+    def save_map(self, date: Union[str, datetime.date], png_data: str) -> None:
+        self.collection.insert_one({"date": self.date2str(date), "png_data": png_data})
 
-  def save_map(self, date: Union[str, datetime.date], png_data: str) -> None:
-    self.collection.insert_one({
-      "date": self.date2str(date),
-      "png_data": png_data
-    })
+    def get_map(self, date: Union[int, datetime.date]) -> Optional[dict]:
+        return self.collection.find_one(
+            {"date": self.date2str(date)}, {"png_data": 1, "_id": 0}
+        )
 
-  def get_map(self, date: Union[int, datetime.date]) -> Optional[dict]:
-    return self.collection.find_one({"date": self.date2str(date)}, {"png_data": 1, "_id": 0})
-
-  def get_dates(self) -> List[dict]:
-    return list(self.collection.find({}, {"date": 1, "_id": 0}).sort("date", ASCENDING))
+    def get_dates(self) -> List[dict]:
+        return list(
+            self.collection.find({}, {"date": 1, "_id": 0}).sort("date", ASCENDING)
+        )
 
 
 db = ROTIMapDB(DB_USER, DB_PASSWORD)
--- backend/src/roti.py
+++ backend/src/roti.py
@@ -22,70 +22,81 @@ MLT = np.linspace(0, 24, 180)
 
 
 class ROTIMap:
+    def __init__(self):
+        self.MLAT = []
+        self.ROTI = []
+        self.DATE = None
 
-  def __init__(self):
-    self.MLAT = []
-    self.ROTI = []
-    self.DATE = None
+    async def read_from_file(self, file: UploadFile) -> None:
+        bytes_obj = await file.read()
+        if file.filename.endswith(".Z"):
+            bytes_obj = unlzw(bytes_obj)
+        self.read_rotimap(bytes_obj)
 
-  async def read_from_file(self, file: UploadFile) -> None:
-    bytes_obj = await file.read()
-    if file.filename.endswith(".Z"):
-      bytes_obj = unlzw(bytes_obj)
-    self.read_rotimap(bytes_obj)
+    def find_rotimap(self, lines: List[bytes]) -> Tuple[int, int]:
+        start_rotimap = None
+        end_rotimap = None
+        for i, line in enumerate(lines):
+            if bytes(START_ROTIMAP, FILE_ENCODING) in line:
+                start_rotimap = i
+            if bytes(END_ROTIMAP, FILE_ENCODING) in line:
+                end_rotimap = i
+        if not start_rotimap:
+            raise Exception(f"'{START_ROTIMAP}' not found")
+        if not end_rotimap:
+            raise Exception(f"'{END_ROTIMAP}' not found")
+        return (start_rotimap, end_rotimap)
 
-  def find_rotimap(self, lines: List[bytes]) -> Tuple[int, int]:
-    start_rotimap = None
-    end_rotimap = None
-    for i, line in enumerate(lines):
-      if bytes(START_ROTIMAP, FILE_ENCODING) in line:
-        start_rotimap = i
-      if bytes(END_ROTIMAP, FILE_ENCODING) in line:
-        end_rotimap = i
-    if not start_rotimap:
-      raise Exception(f"'{START_ROTIMAP}' not found")
-    if not end_rotimap:
-      raise Exception(f"'{END_ROTIMAP}' not found")
-    return (start_rotimap, end_rotimap)
+    def read_rotimap(self, bytes_obj: bytes) -> None:
+        lines = bytes_obj.splitlines()
+        start_rotimap, end_rotimap = self.find_rotimap(lines)
+        self.read_rotimap_date(lines[start_rotimap + 1])
+        for _, group in groupby(
+            lines[start_rotimap + 2 : end_rotimap],
+            key=lambda line: len(self.parse_line(line)) == 3,
+        ):
+            line = b"".join(list(group))
+            parsed_line = self.parse_line(line)
+            if len(parsed_line) == 3:
+                self.MLAT.append(parsed_line[0])
+            else:
+                self.ROTI.append(parsed_line)
 
-  def read_rotimap(self, bytes_obj: bytes) -> None:
-    lines = bytes_obj.splitlines()
-    start_rotimap, end_rotimap = self.find_rotimap(lines)
-    self.read_rotimap_date(lines[start_rotimap + 1])
-    for _, group in groupby(lines[start_rotimap + 2:end_rotimap],
-                            key=lambda line: len(self.parse_line(line)) == 3):
-      line = b"".join(list(group))
-      parsed_line = self.parse_line(line)
-      if len(parsed_line) == 3:
-        self.MLAT.append(parsed_line[0])
-      else:
-        self.ROTI.append(parsed_line)
+    def read_rotimap_date(self, line: bytes) -> None:
+        parsed_line = self.parse_line(line)
+        year = int(parsed_line[0])
+        month = int(parsed_line[1])
+        day = int(parsed_line[2])
+        self.DATE = date(year, month, day)
 
-  def read_rotimap_date(self, line: bytes) -> None:
-    parsed_line = self.parse_line(line)
-    year = int(parsed_line[0])
-    month = int(parsed_line[1])
-    day = int(parsed_line[2])
-    self.DATE = date(year, month, day)
+    def parse_line(self, line: bytes, encoding: str = FILE_ENCODING) -> List[float]:
+        return list(map(float, line.strip().decode(encoding).split()))
 
-  def parse_line(self, line: bytes, encoding: str = FILE_ENCODING) -> List[float]:
-    return list(map(float, line.strip().decode(encoding).split()))
+    def plot(self) -> str:
+        fig = plt.figure()
+        ax = fig.add_subplot(projection="polar")
 
-  def plot(self) -> str:
-    fig = plt.figure()
-    ax = fig.add_subplot(projection="polar")
-    
-    ax.set_title(self.DATE.strftime("%d %B %Y"))
-    ax.set_xticklabels(["06", "", "12", "", "18", "", "00 MLT", ""])
-    ax.set_rgrids([90, 80, 70, 60, 50], angle=45)
-    ax.set_rlim(bottom=90, top=50)
-    
-    theta, r = np.meshgrid(MLT, self.MLAT)
-    cax = ax.contourf(theta, r, self.ROTI, levels=np.linspace(0, 1, 51), cmap="jet", vmin=0, vmax=1)
-    cbar = fig.colorbar(cax, ticks=np.linspace(0, 1, 6), orientation="horizontal", pad=0.1)
-    cbar.set_label("ROTI, TECU/min")
-    
-    with NamedTemporaryFile(suffix=".png") as f:
-      fig.savefig(f, format="png")
-      f.seek(0)
-      return f"data:image/png;base64,{b64encode(f.read()).decode('ascii')}"
+        ax.set_title(self.DATE.strftime("%d %B %Y"))
+        ax.set_xticklabels(["06", "", "12", "", "18", "", "00 MLT", ""])
+        ax.set_rgrids([90, 80, 70, 60, 50], angle=45)
+        ax.set_rlim(bottom=90, top=50)
+
+        theta, r = np.meshgrid(MLT, self.MLAT)
+        cax = ax.contourf(
+            theta,
+            r,
+            self.ROTI,
+            levels=np.linspace(0, 1, 51),
+            cmap="jet",
+            vmin=0,
+            vmax=1,
+        )
+        cbar = fig.colorbar(
+            cax, ticks=np.linspace(0, 1, 6), orientation="horizontal", pad=0.1
+        )
+        cbar.set_label("ROTI, TECU/min")
+
+        with NamedTemporaryFile(suffix=".png") as f:
+            fig.savefig(f, format="png")
+            f.seek(0)
+            return f"data:image/png;base64,{b64encode(f.read()).decode('ascii')}"
